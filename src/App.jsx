c960ba18242ef5f57acb3e6a87c3e2ad461d586d
Prashant Jinwal <98511988+prashantjinwal@users.noreply.github.com>
2025-06-04 22:32:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,7 @@ function App() {
         <h1>PlacePicker</h1>
         <p>
           Create your personal collection of places you would like to visit or
-          you have visited.
+          you have visited....
         </p>
       </header>
       <main>
